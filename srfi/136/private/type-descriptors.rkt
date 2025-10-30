e1f06d9428a1eee5b7bf32e7d427d8b5f65cc2f2
NoahStoryM <NoahStoryM@gmail.com>
2025-10-30 22:21:24 +0800

    [type-descriptors] `optional` -> `deep`

--- srfi/136/private/type-descriptors.rkt
+++ srfi/136/private/type-descriptors.rkt
@@ -1,6 +1,8 @@
-#lang typed/racket/base/optional
+#lang typed/racket/base
 
-(require "types.rkt")
+(require "types.rkt"
+         (rename-in typed/racket/base/optional
+                    [cast unsafe-cast]))
 
 (provide Record-TypeTop
          record:<record>
@@ -17,9 +19,9 @@
 
 (define-new-subtype Record-TypeTop (std->rtd Struct-TypeTop))
 
-(define record:<record> (cast struct:<record> Record-TypeTop))
+(define record:<record> (unsafe-cast struct:<record> Record-TypeTop))
 (define record-type-descriptor?
-  (cast
+  (unsafe-cast
    (λ (v)
      (or (eq? v record:<record>)
          (and (struct-type? v)
@@ -88,13 +90,13 @@
   (for/list ([i : Natural (in-range init-field-cnt)])
     (define name 'unknown)              ; FIXME no `unknown'
     (define (get [rt : <record>])
-      (((cast accessor-proc (→ <record> Integer (→ Any)))
+      (((unsafe-cast accessor-proc (→ <record> Integer (→ Any)))
         rt i)))
     (define set
       (with-handlers ([exn:fail:contract? (λ (_) #f)])
         (make-struct-field-mutator mutator-proc i)
         (λ ([rt : <record>] v)
-          (((cast accessor-proc (→ <record> Integer (→ Any Void)))
+          (((unsafe-cast accessor-proc (→ <record> Integer (→ Any Void)))
             rt i)
            v))))
     (list name get set)))
@@ -116,5 +118,5 @@
 
 (: make-record (→ Record-TypeTop (Vectorof Any) <record>))
 (define (make-record rkd field-vector)
-  (define constructor-proc (cast (record-type-constructor rkd) (→ Any * <record>)))
+  (define constructor-proc (unsafe-cast (record-type-constructor rkd) (→ Any * <record>)))
   (apply constructor-proc (vector->list field-vector)))
